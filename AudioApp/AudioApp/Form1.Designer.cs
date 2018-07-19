54c222f9dd54eba621a0bb32401b9fe824dcdf95
selimhuskic <34183805+selimhuskic@users.noreply.github.com>
2018-07-19 10:45:05 +0200

    Add files via upload

---
+++ AudioApp/AudioApp/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace AudioApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(541, 120);
+            this.panel1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(125, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(285, 34);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Play";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(416, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save/Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(397, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Browse..";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 153);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(541, 79);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(562, 351);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Audio recording";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ AudioApp/AudioApp/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using NAudio.Wave;
+using AudioApp.Helper;
+using WMPLib;
+
+namespace AudioApp
+{
+    public partial class Form1 : Form
+    {
+        public WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Stream myStream = null;
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "audio Files(*.wav;*.mp3;*)|*.wav;*.mp3;|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+        
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+
+                   /*if (!openFileDialog1.FileName.EndsWith("wav") ||
+                            !openFileDialog1.FileName.EndsWith("mp3"))
+                            throw new Exception(message: "You have to choose a wav or mp3 file!");  */
+                            
+                        using (myStream)
+                        {
+                            
+                        }
+                    }
+
+                    textBox1.Text = openFileDialog1.FileName;
+                    wplayer.URL = textBox1.Text;
+                    wplayer.controls.stop();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+             
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                if (textBox1.Text != "")
+                {
+                    wplayer.controls.stop();
+                }
+                
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            try {
+                if(textBox1.Text != "")
+                {                   
+                    wplayer.controls.play();
+                }
+                else
+                    throw new Exception(message: "You have to choose a wav or mp3 file!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+
+
+            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic), "NAudio");
+            Directory.CreateDirectory(outputFolder);
+            string FileName = KeyGeneretor.GetUniqueKey(8);
+            var outputFilePath = Path.Combine(outputFolder, FileName + ".wav");
+
+
+            var waveIn = new WaveInEvent();
+         
+            WaveFileWriter writer = null;
+
+            bool closing = false;
+
+            var format = new WaveFormat(16000, 16, 1);
+
+            writer = new WaveFileWriter(outputFilePath, format);
+            waveIn.StartRecording();
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            /*WaveFileReader reader = new NAudio.Wave.WaveFileReader(outputFilePath);
+            WaveFormat newFormat = new WaveFormat(8000, 16, 1);
+            WaveFormatConversionStream str = new WaveFormatConversionStream(newFormat, reader);*/
+
+
+            waveIn.DataAvailable += (s, a) =>
+            {
+                writer.Write(a.Buffer, 0, a.BytesRecorded);
+                if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 30)
+                {
+                    waveIn.StopRecording();
+                }
+            };
+
+
+            button2.Click += (s, a) => 
+                waveIn.StopRecording();
+
+            FormClosing += (s, a) => { closing = true; waveIn.StopRecording(); };
+
+           
+            waveIn.RecordingStopped += (s, a) =>
+            {
+                writer?.Dispose();
+                writer = null;
+                button1.Enabled = true;
+                button2.Enabled = true;
+
+                if (closing)
+                {
+                    waveIn.Dispose();
+                }
+            };
+            
+        }     
+    }
+}
